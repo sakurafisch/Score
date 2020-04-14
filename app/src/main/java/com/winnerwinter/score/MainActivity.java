e81ee99f8e6b3083b79398a742a659d91b9596d0
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-04-14 21:17:28 +0800

    编写ViewModel并且进行DataBinding

--- app/src/main/java/com/winnerwinter/score/MainActivity.java
+++ app/src/main/java/com/winnerwinter/score/MainActivity.java
@@ -1,14 +1,25 @@
 package com.winnerwinter.score;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
+import com.winnerwinter.score.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    MyViewModel myViewModel;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        myViewModel = ViewModelProviders.of(this).get(MyViewModel.class);
+        binding.setData(myViewModel);
+        binding.setLifecycleOwner(this);
     }
 }
